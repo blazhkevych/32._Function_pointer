9c7fcc0b64894bddf7fcf6ad450f50c2df031083
Виктор Блажкевич <blazhkevych.va@gmail.com>
2022-04-05 20:49:40 +0300

    правки

--- task/lib.cpp
+++ task/lib.cpp
@@ -1,1 +1,101 @@
-﻿
+﻿#include <iostream>
+#include <iomanip>
+
+using std::cout;
+using std::setprecision;
+
+// Функция получает указатель на массив и его размер, и 
+// заполняет массив случайными числами
+void Init(int* ptr, int size, int min, int max)
+{
+	for (int i = 0; i < size; i++)
+	{
+		*ptr = rand() % (max - min + 1) + min;
+		ptr++;
+	}
+}
+
+// Функция получает указатель на массив и его размер, и 
+// заполняет массив случайными числами
+void Init(double* ptr, int size, int min, int max)
+{
+	for (int i = 0; i < size; i++)
+	{
+		*ptr = (rand() % (max - min + 1) + min) * 0.99;
+		ptr++;
+	}
+}
+
+// Функция получает указатель на массив и его размер, и 
+// выводит массив на экран
+void Print(int* ptr, int size)
+{
+	for (int i = 0; i < size; i++)
+	{
+		cout << ptr[i] << '\t';
+	}
+}
+
+// Функция получает указатель на массив и его размер, и 
+// выводит массив на экран
+void Print(double* ptr, int size)
+{
+	for (int i = 0; i < size; i++)
+	{
+		cout << setprecision(4) << ptr[i] << '\t';
+	}
+}
+
+// функция создания динамического массива
+double* Allocate(int size)
+{
+	return new double[size];
+}
+
+// функция удаления динамического массива
+void Free(double* ptr)
+{
+	delete[] ptr;
+}
+
+// Функция находит максимальное число в массиве.
+double Max(double* ptrArr, int size)
+{
+	double max = ptrArr[0];
+	for (int i = 0; i < size; i++)
+	{
+		if (ptrArr[i] > max)
+			max = ptrArr[i];
+	}
+	return max;
+}
+
+// Функция находит минимальное число в массиве.
+double Min(double* ptrArr, int size)
+{
+	double min = ptrArr[0];
+	for (int i = 0; i < size; i++)
+	{
+		if (ptrArr[i] < min)
+			min = ptrArr[i];
+	}
+	return min;
+}
+
+// Функция ищет среднее арифметическое элементов массива.
+double Avg(double* ptrArr, int size)
+{
+	double sum{ 0.0 };
+	for (int i = 0; i < size; i++)
+	{
+		sum = sum + ptrArr[i];
+	}
+	return sum / size;
+}
+
+// Функция принимает в качестве параметров указатель на массив типа double, размер массива, а
+// также указатель на функцию.
+double Action(double* ptrArr, int size, double(*ptrAction)(double*, int))
+{
+	return ptrAction(ptrArr, size);
+}
--- task/task.cpp
+++ task/task.cpp
@@ -1,22 +1,73 @@
-﻿	/*
-							Указатель на функцию
+﻿/*
+						Указатель на функцию
 	Написать программу, которая содержит функцию Action, принимающую
-	в качестве параметров указатель на массив типа double, размер массива, а
-	также указатель на функцию. Программа должна отобразить пользователю
-	меню, в котором он может выбрать один из трех пунктов: maximum, minimum,
-	average. Если выбран пункт maximum, то в функцию Action передается адрес
-	функции, которая ищет максимум в массиве. Если выбран пункт minimum, то
-	в функцию Action передается адрес функции, которая ищет минимум в
-	массиве. Если выбран пункт average, то в функцию Action передается адрес
-	функции, которая ищет среднее арифметическое элементов массива.
-	Возвращаемое значение функции Action – результат, который вернет одна из
-	этих трех функций (maximum, minimum, average). Все три функции должны
-	принимать в качестве параметров указатель на массив типа double, а также
-	размер массива. Функции должны возвращать результат типа double
-	*/
+в качестве параметров указатель на массив типа double, размер массива, а
+также указатель на функцию. Программа должна отобразить пользователю
+меню, в котором он может выбрать один из трех пунктов: maximum, minimum,
+average. Если выбран пункт maximum, то в функцию Action передается адрес
+функции, которая ищет максимум в массиве. Если выбран пункт minimum, то
+в функцию Action передается адрес функции, которая ищет минимум в
+массиве. Если выбран пункт average, то в функцию Action передается адрес
+функции, которая ищет среднее арифметическое элементов массива.
+Возвращаемое значение функции Action – результат, который вернет одна из
+этих трех функций (maximum, minimum, average). Все три функции должны
+принимать в качестве параметров указатель на массив типа double, а также
+размер массива. Функции должны возвращать результат типа double
+*/
+//2.51.19
 #include <iostream>
+#include "lib.h"
+using std::cout;
+using std::endl;
+using std::cin;
 
 int main()
 {
+	srand(time(0));
+	char answer{ 0 };
 
+	do
+	{
+		int size{ 5 };
+		double* ptrArr = Allocate(size);
+		Init(ptrArr, size, 1.00, 99.00);
+		Print(ptrArr, size);
+		cout << endl;
+		double(*ptrAction)(double*, int) { nullptr };
+
+		int choice{ 0 };
+		cout << "Enter number: "
+			<< "\n1. Minimum"
+			<< "\n2. Maximum"
+			<< "\n3. Average"
+			<< endl;
+		cin >> choice;
+
+		switch (choice)
+		{
+		case 1: // Minimum
+			ptrAction = Min;
+			cout << Action(ptrArr, size, ptrAction);
+			break;
+
+		case 2: // Maximum
+			ptrAction = Max;
+			cout << Action(ptrArr, size, ptrAction);
+			break;
+
+		case 3: // Average
+			ptrAction = Avg;
+			cout << Action(ptrArr, size, ptrAction);
+			break;
+		}
+
+		Free(ptrArr);
+		cout << endl;
+		cout << "Do you want to continue? ( y (yes) / n (no) )\n";
+		cin >> answer;
+		cout << endl;
+
+	} while (answer == 'y');
+
+	return 0;
 }
